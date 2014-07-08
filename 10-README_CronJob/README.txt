a316f14ae669c9701aa615325f27c0293494c774
Patrick Merlot <patrick.merlot@gmail.com>
2014-07-08 13:31:35 +0200

    adding a simple cronjob example running from Ubuntu on my machine every day at 13:32

---
+++ 10-README_CronJob/README.txt
@@ -0,0 +1,12 @@
+Ubuntu official documentation on Cron jobs: https://help.ubuntu.com/community/CronHowto
+
+
+GUI:
+installation: sudo apt-get install gnome-schedule
+launch it with: gnome-schedule
+
+
+open the crontab file using this command:
+> crontab -e
+
+and in the crontab file, add such a line:
---
+++ 10-README_CronJob/dailyScript.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+NOW=`date +'%Y_%m_%dT%H%M'`
+
+DIR_ROOT=/tmp
+NAME_CLONE="CMake-CTest-CDash_$NOW"
+
+DIR_CLONE=$DIR_ROOT/$NAME_CLONE
+rm -rf $DIR_CLONE
+cd /tmp
+pwd
+git clone git@github.com:Patechoc/CMake-CTest-CDash.git $NAME_CLONE
+cd $DIR_CLONE/09-Adding_Support_for_Dashboard
+pwd
+mkdir myBuild
+cd myBuild
+cmake -DCMAKE_INSTALL_PREFIX="../myInstallation" ..
+make
+make install
+ctest -D Nightly
+ctest -D Nightly -R Comp
